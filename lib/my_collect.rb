8814faa0e688e2f8db4825fcf41b396c9e2681d4
Andrew Sprague <andrewsprague@outlook.com>
2018-08-15 02:40:53 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect (array) 
+  while i < array.length
+    
+  end
+end
 
+my_collect (languages) do |lang|
+  lang.upcase
+end
